23fa47e0d53a8a2af7e3e2de9d1d6be6fec3a39b
Nick Ko <nicholasedwardko@gmail.com>
2024-05-17 17:25:37 +0100

    feat: refactored validateName to use regex.

--- src/main/java/com/addressbook/app/Validate.java
+++ src/main/java/com/addressbook/app/Validate.java
@@ -2,10 +2,12 @@ package com.addressbook.app;
 
 public abstract class Validate {
 
+    public static final String nameRegex = "^[a-zA-Z]{2,} [a-zA-Z]+$";
+
     public static boolean validateName(String name) {
-        if(name == null || name.trim().isEmpty()){
-            throw new IllegalArgumentException("Name cannot be null or empty.");
-        };
+        if (!name.matches(nameRegex)){
+            throw new IllegalArgumentException("Please enter a valid name. ");
+        }
         return true;
     }
 }
--- src/test/java/com/addressbook/app/ValidateTest.java
+++ src/test/java/com/addressbook/app/ValidateTest.java
@@ -4,26 +4,46 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DisplayName("Validate class tests")
 public class ValidateTest {
 
     @Nested
-    class testStrings {
-        String validName = "Nick";
+    class testNameStrings {
+        String validName = "Nick Ko";
         String invalidName = "123";
         String whiteSpace = "  ";
 
+        @Test
+        @DisplayName("Test validateName returns true if valid")
+        void testValidateNameReturnsTrue() {
+            // Arrange
+            // Act
+            // Assert
+            assertTrue(Validate.validateName(validName));
+        }
 
-    @Test
-    @DisplayName("Test validateName returns true if valid")
-    void testValidateNameReturnsTrue() {
-        // Arrange
-        // Act
-        // Assert
-        assertTrue(Validate.validateName(validName));
+        @Test
+        @DisplayName("Test validateName throws error if whiteSpace")
+        void testValidateNameThrowsErrorIfWhiteSpace() {
+            // Arrange
+            // Act
+            // Assert
+            assertThrows(IllegalArgumentException.class,
+                    () -> Validate.validateName(whiteSpace));
+        }
+
+
+//    @Nested
+//    class testEmailStrings {
+//        String validEmail = "nick@email.com";
+//        String isNull = null;
+//        String emailRegex = "/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g";
+//
+//    @Test
+//        @DisplayName("")
+//    }
     }
-
-}
 }
