72f4834ccb2a651642ef4c4d71eef2945411c4c8
Nick Ko <nicholasedwardko@gmail.com>
2024-07-22 12:36:20 +0100

    chore: refractor App.java

--- src/main/java/com/addressbook/app/App.java
+++ src/main/java/com/addressbook/app/App.java
@@ -16,71 +16,12 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        boolean applicationOn = true;
         AddressBook addressBook = new AddressBook();
         Scanner scanner = new Scanner(System.in);
 
-        while (applicationOn) {
-            MenuPrinter.displayMenu();
-            String userSelection = UserInput.captureMenuInput(scanner);
-
-
-            switch (userSelection) {
-                case "1":
-                    MenuPrinter.menuHeader("CONTACT LIST");
-                    addressBook.viewContacts();
-                    break;
-                case "2":
-                    MenuPrinter.menuHeader("ADD A NEW CONTACT");
-                    try {
-                        String nameInput = UserInput.captureInput("Enter contact's full name:", scanner);
-                        String emailInput = UserInput.captureInput("Enter contact's email address:", scanner);
-                        String phoneInput = UserInput.captureInput("Enter contact's phone number (11 digit mobile - e.g. 07XXXXXXXXX):", scanner);
-                        addressBook.addContact(new Contact(nameInput, emailInput, phoneInput));
-                    } catch (IllegalArgumentException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case "3":
-                    MenuPrinter.menuHeader("SEARCH CONTACTS");
-                    try {
-                        System.out.println("Enter a name to search for:");
-                        String searchInput = scanner.nextLine();
-                        System.out.println(UserInput.searchList(addressBook.getContacts(), searchInput));
-                    } catch (RuntimeException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case "4":
-                    MenuPrinter.menuHeader("EDIT CONTACT");
-                    try {
-                        String nameInput = UserInput.captureInput("Which contact would you like to edit? Enter their full name:", scanner);
-                        String newNameInput = UserInput.captureInput("Enter contact's new Name:", scanner);
-                        String newEmailInput = UserInput.captureInput("Enter contact's new Email:", scanner);
-                        String newPhoneNumberInput = UserInput.captureInput("Enter contact's new PhoneNumber (11 digit mobile - e.g. 07XXXXXXXXX):", scanner);
-                        addressBook.editContact(nameInput, newNameInput, newEmailInput, newPhoneNumberInput);
-                        System.out.printf("Successfully edited contact's details to: %s, %s, %s %n", newNameInput, newEmailInput, newPhoneNumberInput);
-                    } catch (RuntimeException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-
-                case "5":
-                    MenuPrinter.menuHeader("REMOVE CONTACT");
-                    try {
-                        String toRemoveInput = UserInput.captureInput("Which contact would you like to remove? Enter their full name:", scanner);
-                        addressBook.removeContact(toRemoveInput);
-                    } catch (RuntimeException e) {
-                        System.out.println(e.getMessage());
-                    }
-                    break;
-                case "6":
-                    System.out.println("Closing the address book, until next time!");
-                    scanner.close();
-                    applicationOn = false;
-            }
-        }
+        UserInput.start(scanner, addressBook);
     }
 }
+
 
 
--- src/main/java/com/addressbook/app/UserInput.java
+++ src/main/java/com/addressbook/app/UserInput.java
@@ -27,9 +27,9 @@ public class UserInput {
             try {
                 userInput = scanner.nextLine();
                 Validate.validateUserInputMenu(userInput);
-                return userInput; // Return the validated input
+                return userInput;
             } catch (RuntimeException e) {
-                System.out.println(e.getMessage()); // Print error message and prompt again
+                System.out.println(e.getMessage());
                 System.out.println("Please enter a valid selection:");
             }
         }
@@ -39,8 +39,73 @@ public class UserInput {
         System.out.println(prompt);
         String input = scanner.nextLine();
         Validate.validateUserInput(input);
-        return input; // Return the validated input
+        return input;
     }
 
+    public static void start(Scanner scanner, AddressBook addressBook) {
+
+        boolean applicationOn = true;
+
+        while (applicationOn) {
+            MenuPrinter.displayMenu();
+            String userSelection = UserInput.captureMenuInput(scanner);
+
+            switch (userSelection) {
+                case "1":
+                    MenuPrinter.menuHeader("CONTACT LIST");
+                    addressBook.viewContacts();
+                    break;
+                case "2":
+                    MenuPrinter.menuHeader("ADD A NEW CONTACT");
+                    try {
+                        String nameInput = UserInput.captureInput("Enter contact's full name:", scanner);
+                        String emailInput = UserInput.captureInput("Enter contact's email address:", scanner);
+                        String phoneInput = UserInput.captureInput("Enter contact's phone number (11 digit mobile - e.g. 07XXXXXXXXX):", scanner);
+                        addressBook.addContact(new Contact(nameInput, emailInput, phoneInput));
+                    } catch (IllegalArgumentException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "3":
+                    MenuPrinter.menuHeader("SEARCH CONTACTS");
+                    try {
+                        System.out.println("Enter a name to search for:");
+                        String searchInput = scanner.nextLine();
+                        System.out.println(UserInput.searchList(addressBook.getContacts(), searchInput));
+                    } catch (RuntimeException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "4":
+                    MenuPrinter.menuHeader("EDIT CONTACT");
+                    try {
+                        String nameInput = UserInput.captureInput("Which contact would you like to edit? Enter their full name:", scanner);
+                        String newNameInput = UserInput.captureInput("Enter contact's new Name:", scanner);
+                        String newEmailInput = UserInput.captureInput("Enter contact's new Email:", scanner);
+                        String newPhoneNumberInput = UserInput.captureInput("Enter contact's new PhoneNumber (11 digit mobile - e.g. 07XXXXXXXXX):", scanner);
+                        addressBook.editContact(nameInput, newNameInput, newEmailInput, newPhoneNumberInput);
+                        System.out.printf("Successfully edited contact's details to: %s, %s, %s %n", newNameInput, newEmailInput, newPhoneNumberInput);
+                    } catch (RuntimeException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+
+                case "5":
+                    MenuPrinter.menuHeader("REMOVE CONTACT");
+                    try {
+                        String toRemoveInput = UserInput.captureInput("Which contact would you like to remove? Enter their full name:", scanner);
+                        addressBook.removeContact(toRemoveInput);
+                    } catch (RuntimeException e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case "6":
+                    System.out.println("Closing the address book, until next time!");
+                    scanner.close();
+                    applicationOn = false;
+            }
+        }
+
     }
+}
 
