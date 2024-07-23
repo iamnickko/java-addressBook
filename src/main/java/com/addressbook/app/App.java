fe5536d59d4c4e3f0717cfea6e63a6c6855611d1
Nick Ko <nicholasedwardko@gmail.com>
2024-07-23 21:39:27 +0100

    chore: removed submission related comments in App.java

--- src/main/java/com/addressbook/app/App.java
+++ src/main/java/com/addressbook/app/App.java
@@ -2,18 +2,6 @@ package com.addressbook.app;
 
 import java.util.Scanner;
 
-/* Hello, Ed! A few things I'd like to highlight that I'd improve on given a bit more time and to show how the direction I'd
-    have continued working:
-* - Would definitely pull out each case in the switch block and have as its own function in InputScanner. I believe I could
-*   then write unit tests and increase the test coverage. Would apply relevant Validate functions to each.
-* - I'd pull out the formatting System.out.println("=====") and the menu titles into their own function to reuse.
-* - I'd move editContact() from AddressBook into Contact - better single responsibility. Contact for creating & editing,
-*   AddressBook for holding, removing, & viewing.
-* - Testing suites could be refactored with beforeEach & afterEach. I also struggled with how to make mocks behave how I
-*   wanted, particularly with creating a mock AddressBook, so there's a couple of instances of tightly coupling other
-*   classes to tests. */
-
-
 public class App {
     public static void main(String[] args) {
         AddressBook addressBook = new AddressBook();
